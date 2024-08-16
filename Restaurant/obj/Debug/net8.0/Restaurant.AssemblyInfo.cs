de73bb5ec6a3ae1a50116b78a6754f54cb2dde66
Alaa Mobarak <3laa.m0o0barak@gmail.com>
2024-08-16 18:12:34 +0300

    CRUD controllers

--- Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
+++ Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03fa095f6cd64b82f72c5fa63ca0211c5c77eee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de09071061046622a102500feb32c41bb83dc90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
